193842c78602766205752bf4f3c1f1f6b99a2836
q3689852 <3689852@qq.com>
2017-03-06 10:35:19 +0800

    顺序的牌

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -45,91 +45,77 @@ int main()
 		}
 	}
 	 
-	//全部的牌
-	/*for (int i = 0; i < 54; i++)
-	cout << all[i] << endl;*/
 
-	
-	
-
-
-
-	//发牌
-	int a_p = 0;					//player1数组下标
-	int b_p = 0;					//player2数组下标
-	int c_p = 0;					//player3数组下标
-		for (int i = 0; i< 54; i++)
-		{	
-			int who = i % 3;
-			switch (who)
-			{	
-				case 0:
-				{player1[a_p] = all[i];++a_p;}break;
-				case 1:
-				{player2[b_p] = all[i];++b_p;}	break;
-				case 2:
-				{player3[c_p] = all[i];++c_p;}	break;
-			}
-		}
-	
-		//展示
-
-		show(player1,18,"一");
-		show(player2,18,"二");
-		show(player3,18,"三");
-
-		//排序
+		//顺序all数组
 		string HEI[13];
 		string HONG[13];
 		string MEI[13];
 		string FANG[13];
 		string GUI[2];
 		for (int i = 0; i < 13; i++)
-		{
-		HEI[i]=all[i];
-		}
-		
+			HEI[i] = all[i];
+		for (int i = 0; i < 13; i++)		
+			HONG[i] = all[13 + i];		
+		for (int i = 0; i < 13; i++)		
+			MEI[i] = all[26 + i];	
+		for (int i = 0; i < 13; i++)	
+			FANG[i] = all[39 + i];
+		for (int i = 0; i < 2; i++)
+			GUI[i] = all[52 + i];
+
 		for (int i = 0; i < 13; i++)
 		{
-			HONG[i] = all[ i+13];
-		}
-		
+			all[0+4*i] = HEI[i];
+		}	
 		for (int i = 0; i < 13; i++)
 		{
-			MEI[i] = all[i + 26];
+			all[1+4*i] = HONG[i];
 		}
-		
 		for (int i = 0; i < 13; i++)
 		{
-			FANG[i] = all[i + 39];
-		}
-		
-		show(HEI, 13, "黑桃");
-		show(HONG, 13, "红桃");
-		show(MEI, 13, "梅花");
-		show(FANG, 13, "梅花");
-		cout << all[53] << endl;
-//	show(GUI, 2, "王");
+			all[2+4*i] = MEI[i];
 
-
-
-	/*	for (int i = 0; i < 26; i++)
-		{
-		HONG[i]=all[26 + i];
 		}
-		for (int i = 0; i < 39; i++)
+		for (int i = 0; i < 13; i++)
 		{
-		MEI[i] = all[39 + i];
+			all[3+4*i] = FANG[i];
 		}
-		for (int i = 0; i < 52; i++)
+		for (int i = 0; i < 2; i++)
 		{
-		FANG[i] = all[52 + i];
-		}*/
-		//	
-		//	show(HONG, "红桃");
-		//	show(MEI , "梅花");
-		//	show(MEI, "方片");
-		//show(FANG , "A");
+			all[i + 52] = GUI[i];
+		}
+		cout << "_______________________顺序的牌___________________________" << endl;
+		for (int i = 0; i < 54; i++)
+		{
+			cout << all[i] << "\t" ;
+			if (i % 4 > 2)
+				cout << endl;
+		}
+		cout << endl << "_______________________结束_______________________________" << endl;
+
+		//发牌
+		int a_p = 0;					//player1数组下标
+		int b_p = 0;					//player2数组下标
+		int c_p = 0;					//player3数组下标
+		for (int i = 0; i< 54; i++)
+		{
+			int who = i % 3;
+			switch (who)
+			{
+			case 0:
+			{player1[a_p] = all[i]; ++a_p; }break;
+			case 1:
+			{player2[b_p] = all[i]; ++b_p; }	break;
+			case 2:
+			{player3[c_p] = all[i]; ++c_p; }	break;
+			}
+		}
+
+		//展示
+
+		show(player1, 18, "玩家一");
+		show(player2, 18, "玩家二");
+		show(player3, 18, "玩家三");
 
 
 			return 0;
